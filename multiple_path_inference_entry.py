a3a140bbe932c371aff94cdc7d9173fbd665b2e9
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-04-28 10:55:47 +0200

    Multipath inference

--- multiple_path_inference_entry.py
+++ multiple_path_inference_entry.py
@@ -18,7 +18,7 @@ class MultiplePathOptimizer(object):
 
 
 if __name__ == "__main__":
-    DbLogger.log_db_path = DbLogger.home_asus
+    DbLogger.log_db_path = DbLogger.jr_cigt
     normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
