32e08e99c7e035d20eb6338f3c84dbb1c5ebb567
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-07-11 16:50:44 +0200

    Adding Gumbel Softmax

--- bayesian_multipath_optimization_entry.py
+++ bayesian_multipath_optimization_entry.py
@@ -27,21 +27,25 @@ from cigt.cutout_augmentation import CutoutPIL
 from cigt.multipath_inference_bayesian import MultiplePathBayesianOptimizer
 from cigt.resnet_cigt_constants import ResnetCigtConstants
 import torch
+import random
 
 from cigt.softmax_decay_algorithms.step_wise_decay_algorithm import StepWiseDecayAlgorithm
+
+# random.seed(53)
+# np.random.seed(61)
 
 if __name__ == "__main__":
     print("X")
     # 5e-4,
     # 0.0005
     ResnetCigtConstants.resnet_config_list = [
-            {"path_count": 1,
-                    "layer_structure": [{"layer_count": 9, "feature_map_count": 16}]},
-            {"path_count": 2,
-                    "layer_structure": [{"layer_count": 9, "feature_map_count": 12},
-                                           {"layer_count": 18, "feature_map_count": 16}]},
-            {"path_count": 4,
-                    "layer_structure": [{"layer_count": 18, "feature_map_count": 16}]}]
+        {"path_count": 1,
+         "layer_structure": [{"layer_count": 9, "feature_map_count": 16}]},
+        {"path_count": 2,
+         "layer_structure": [{"layer_count": 9, "feature_map_count": 12},
+                             {"layer_count": 18, "feature_map_count": 16}]},
+        {"path_count": 4,
+         "layer_structure": [{"layer_count": 18, "feature_map_count": 16}]}]
     ResnetCigtConstants.classification_wd = 0.0005
     ResnetCigtConstants.information_gain_balance_coeff_list = [5.0, 5.0]
     ResnetCigtConstants.loss_calculation_kind = "MultipleLogitsMultipleLosses"
@@ -86,10 +90,10 @@ if __name__ == "__main__":
         batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
 
     chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
-                                     "checkpoints/dblogger2_94_epoch1390.pth")
+                             "checkpoints/dblogger2_94_epoch1390.pth")
     data_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "dblogger2_94_epoch1390_data")
 
-    DbLogger.log_db_path = DbLogger.home_asus
+    DbLogger.log_db_path = DbLogger.jr_cigt
 
     run_id = DbLogger.get_run_id()
     model = CigtIgGatherScatterImplementation(
@@ -111,14 +115,6 @@ if __name__ == "__main__":
         train_dataset_repeat_count=10,
         evaluate_network_first=False,
         model=model)
-
-
-
-
-
-
-
-
 
     # # weight_decay = 5 * [0.0, 0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005]
     # weight_decay = 10 * [0.0005]
@@ -163,89 +159,88 @@ if __name__ == "__main__":
     #
     #     run_id = DbLogger.get_run_id()
 
-        # ResnetCigtConstants.loss_calculation_kind = "SingleLogitSingleLoss"
-        # teacher_model = CigtIdealRouting(
-        #     run_id=run_id,
-        #     model_definition="Cigt Ideal Routing - [1,2,4] - SingleLogitSingleLoss - Wd:0.0005",
-        #     num_classes=10,
-        #     class_to_route_mappings=
-        #     [[(5, 6, 4, 7, 3, 2), (1, 8, 9, 0)],
-        #      [(6, 0), (1, 8, 9), (5, 7, 3), (4, 2)]])
+    # ResnetCigtConstants.loss_calculation_kind = "SingleLogitSingleLoss"
+    # teacher_model = CigtIdealRouting(
+    #     run_id=run_id,
+    #     model_definition="Cigt Ideal Routing - [1,2,4] - SingleLogitSingleLoss - Wd:0.0005",
+    #     num_classes=10,
+    #     class_to_route_mappings=
+    #     [[(5, 6, 4, 7, 3, 2), (1, 8, 9, 0)],
+    #      [(6, 0), (1, 8, 9), (5, 7, 3), (4, 2)]])
 
-        # Cifar 10 Dataset
-        # kwargs = {'num_workers': 2, 'pin_memory': True}
-        # test_loader = torch.utils.data.DataLoader(
-        #     datasets.CIFAR10('../data', train=False, transform=teacher_model.transformTest),
-        #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
-        # teacher_model.validate(loader=test_loader, epoch=0, data_kind="test")
+    # Cifar 10 Dataset
+    # kwargs = {'num_workers': 2, 'pin_memory': True}
+    # test_loader = torch.utils.data.DataLoader(
+    #     datasets.CIFAR10('../data', train=False, transform=teacher_model.transformTest),
+    #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
+    # teacher_model.validate(loader=test_loader, epoch=0, data_kind="test")
 
-        # ResnetCigtConstants.loss_calculation_kind = "MultipleLogitsMultipleLosses"
-        # model = CigtIgWithKnowledgeDistillation(
-        #     run_id=run_id,
-        #     model_definition="KD Cigt - [1,2,4] - MultipleLogitsMultipleLosses - Wd:0.0005 - use_kd_for_routing = False - kd_teacher_temperature = 10.0 - kd_loss_alpha = 0.95",
-        #     num_classes=10, teacher_model=teacher_model)
-        # model.modelFilesRootPath = ResnetCigtConstants.model_file_root_path_hpc
-        # explanation = model.get_explanation_string()
-        # DbLogger.write_into_table(rows=[(run_id, explanation)], table=DbLogger.runMetaData)
+    # ResnetCigtConstants.loss_calculation_kind = "MultipleLogitsMultipleLosses"
+    # model = CigtIgWithKnowledgeDistillation(
+    #     run_id=run_id,
+    #     model_definition="KD Cigt - [1,2,4] - MultipleLogitsMultipleLosses - Wd:0.0005 - use_kd_for_routing = False - kd_teacher_temperature = 10.0 - kd_loss_alpha = 0.95",
+    #     num_classes=10, teacher_model=teacher_model)
+    # model.modelFilesRootPath = ResnetCigtConstants.model_file_root_path_hpc
+    # explanation = model.get_explanation_string()
+    # DbLogger.write_into_table(rows=[(run_id, explanation)], table=DbLogger.runMetaData)
 
-        # model = CigtMaskedRouting(
-        #     run_id=run_id,
-        #     model_definition="Masked Cigt - [1,2,4] - [5.0, 5.0] - MultipleLogitsMultipleLosses - Wd:0.0005 - 350 "
-        #                      "Epoch Warm "
-        #                      "up with: RandomRoutingButInformationGainOptimizationEnabled - "
-        #                      "InformationGainRoutingWithRandomization - apply_mask_to_batch_norm: True",
-        #     num_classes=10)
+    # model = CigtMaskedRouting(
+    #     run_id=run_id,
+    #     model_definition="Masked Cigt - [1,2,4] - [5.0, 5.0] - MultipleLogitsMultipleLosses - Wd:0.0005 - 350 "
+    #                      "Epoch Warm "
+    #                      "up with: RandomRoutingButInformationGainOptimizationEnabled - "
+    #                      "InformationGainRoutingWithRandomization - apply_mask_to_batch_norm: True",
+    #     num_classes=10)
 
-        # chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
-        #                                  "checkpoints/dblogger_141_epoch1370.pth")
-        # _141_checkpoint = torch.load(chck_path, map_location="cpu")
-        # model.load_state_dict(state_dict=_141_checkpoint["model_state_dict"])
+    # chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
+    #                                  "checkpoints/dblogger_141_epoch1370.pth")
+    # _141_checkpoint = torch.load(chck_path, map_location="cpu")
+    # model.load_state_dict(state_dict=_141_checkpoint["model_state_dict"])
 
-
-        # model = CigtBayesianMultipath(
-        #     run_id=run_id,
-        #     model_definition="Gather Scatter Cigt With CBAM Routers With Random Augmentation - cbam_layer_input_reduction_ratio:4  - [1,2,4] - [5.0, 5.0] - number_of_cbam_layers_in_routing_layers:3 - MultipleLogitsMultipleLosses - Wd:0.0005 - 350 Epoch Warm up with: RandomRoutingButInformationGainOptimizationEnabled - InformationGainRoutingWithRandomization",
-        #     num_classes=10)
-        #
-        # chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
-        #                                  "checkpoints/dblogger2_94_epoch1390.pth")
-        # cbam_checkpoint = torch.load(chck_path, map_location="cpu")
-        # model.load_state_dict(state_dict=cbam_checkpoint["model_state_dict"], strict=False)
-        #
-        # model.modelFilesRootPath = ResnetCigtConstants.model_file_root_path_tetam_tuna
-        # explanation = model.get_explanation_string()
-        # DbLogger.write_into_table(rows=[(run_id, explanation)], table=DbLogger.runMetaData)
-        #
-        # heavyweight_augmentation = transforms.Compose([
-        #     transforms.Resize(model.imageSize),
-        #     CutoutPIL(cutout_factor=0.5),
-        #     RandAugment(),
-        #     transforms.ToTensor(),
-        # ])
-        # lightweight_augmentation = transforms.Compose([
-        #     transforms.Resize(model.imageSize),
-        #     transforms.ToTensor(),
-        # ])
-        #
-        # # Cifar 10 Dataset
-        # kwargs = {'num_workers': 2, 'pin_memory': True}
-        # train_loader_hard = torch.utils.data.DataLoader(
-        #     datasets.CIFAR10('../data', train=True, transform=heavyweight_augmentation),
-        #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
-        #
-        # train_loader_light = torch.utils.data.DataLoader(
-        #     datasets.CIFAR10('../data', train=True, transform=lightweight_augmentation),
-        #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
-        #
-        # test_loader = torch.utils.data.DataLoader(
-        #     datasets.CIFAR10('../data', train=False, transform=lightweight_augmentation),
-        #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
-        #
-        # # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
-        # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
-        # model.validate(loader=train_loader_hard, data_kind="train", epoch=0, temperature=0.1)
-        # model.validate(loader=train_loader_light, data_kind="train", epoch=0, temperature=0.1)
-        # print("X")
-        #
-        # # model.fit_temperatures_with_respect_to_variances()
-        # # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
+    # model = CigtBayesianMultipath(
+    #     run_id=run_id,
+    #     model_definition="Gather Scatter Cigt With CBAM Routers With Random Augmentation - cbam_layer_input_reduction_ratio:4  - [1,2,4] - [5.0, 5.0] - number_of_cbam_layers_in_routing_layers:3 - MultipleLogitsMultipleLosses - Wd:0.0005 - 350 Epoch Warm up with: RandomRoutingButInformationGainOptimizationEnabled - InformationGainRoutingWithRandomization",
+    #     num_classes=10)
+    #
+    # chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
+    #                                  "checkpoints/dblogger2_94_epoch1390.pth")
+    # cbam_checkpoint = torch.load(chck_path, map_location="cpu")
+    # model.load_state_dict(state_dict=cbam_checkpoint["model_state_dict"], strict=False)
+    #
+    # model.modelFilesRootPath = ResnetCigtConstants.model_file_root_path_tetam_tuna
+    # explanation = model.get_explanation_string()
+    # DbLogger.write_into_table(rows=[(run_id, explanation)], table=DbLogger.runMetaData)
+    #
+    # heavyweight_augmentation = transforms.Compose([
+    #     transforms.Resize(model.imageSize),
+    #     CutoutPIL(cutout_factor=0.5),
+    #     RandAugment(),
+    #     transforms.ToTensor(),
+    # ])
+    # lightweight_augmentation = transforms.Compose([
+    #     transforms.Resize(model.imageSize),
+    #     transforms.ToTensor(),
+    # ])
+    #
+    # # Cifar 10 Dataset
+    # kwargs = {'num_workers': 2, 'pin_memory': True}
+    # train_loader_hard = torch.utils.data.DataLoader(
+    #     datasets.CIFAR10('../data', train=True, transform=heavyweight_augmentation),
+    #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
+    #
+    # train_loader_light = torch.utils.data.DataLoader(
+    #     datasets.CIFAR10('../data', train=True, transform=lightweight_augmentation),
+    #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
+    #
+    # test_loader = torch.utils.data.DataLoader(
+    #     datasets.CIFAR10('../data', train=False, transform=lightweight_augmentation),
+    #     batch_size=ResnetCigtConstants.batch_size, shuffle=False, **kwargs)
+    #
+    # # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
+    # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
+    # model.validate(loader=train_loader_hard, data_kind="train", epoch=0, temperature=0.1)
+    # model.validate(loader=train_loader_light, data_kind="train", epoch=0, temperature=0.1)
+    # print("X")
+    #
+    # # model.fit_temperatures_with_respect_to_variances()
+    # # model.validate(loader=test_loader, epoch=0, data_kind="test", temperature=0.1)
--- cigt/multipath_inference_bayesian.py
+++ cigt/multipath_inference_bayesian.py
@@ -7,6 +7,7 @@ from auxillary.db_logger import DbLogger
 from auxillary.utilities import Utilities
 from cigt.cigt_ig_gather_scatter_implementation import CigtIgGatherScatterImplementation
 from cigt.resnet_cigt_constants import ResnetCigtConstants
+from tqdm import tqdm
 
 
 class NetworkOutput(object):
@@ -38,48 +39,47 @@ class MultiplePathBayesianOptimizer(BayesianOptimizer):
             test_acc = self.model.validate(data_kind="test", epoch=0, loader=self.testDataset, temperature=0.1)
             print("Standard test accuracy:{0}".format(test_acc))
 
-        self.assert_gather_scatter_model_output_correctness(dataloader=self.testDataset, repeat_count=1)
-        # self.create_outputs(dataloader=self.trainDataset, repeat_count=self.repeatCount)
-        # self.create_outputs(dataloader=self.testDataset, repeat_count=1)
+        # self.assert_gather_scatter_model_output_correctness(dataloader=self.testDataset, repeat_count=1)
+        self.create_outputs(dataloader=self.trainDataset, repeat_count=self.repeatCount)
+        self.create_outputs(dataloader=self.testDataset, repeat_count=1)
 
-    def get_start_offset_for_gather_scatter_model(self, route_, batch_id_, model_batch_size, curr_batch_size):
+    def get_start_offset_for_gather_scatter_model(self, route_, batch_id_, curr_batch_size):
         path_list = list(zip(list(range(len(route_))), route_))
-        batch_size = self.model.batchSize
-        offset = batch_id_ * np.prod(self.model.pathCounts[:len(route_)], dtype=np.int64) * batch_size
+        offset = batch_id_ * np.prod(self.model.pathCounts[:len(route_)], dtype=np.int64) * self.model.batchSize
         for tpl in reversed(path_list):
             block_id = tpl[0]
             block_path_index = tpl[1]
-            offset_step = block_path_index * np.prod(self.model.pathCounts[:block_id],
-                                                     dtype=np.int64) * batch_size
+            offset_step = block_path_index * np.prod(self.model.pathCounts[:block_id], dtype=np.int64) * curr_batch_size
             offset += offset_step
         return offset
 
     def interpret_gather_scatter_model_outputs(self, outputs_dict, dataloader):
         network_output = NetworkOutput()
-        batch_size = outputs_dict["list_of_labels"][0].shape[0]
+        data_size = outputs_dict["list_of_labels"][0].shape[0]
         for block_id in range(len(self.model.pathCounts)):
             # Routing blocks
             if block_id < len(self.model.pathCounts) - 1:
                 output_array = outputs_dict["list_of_routing_activations"][block_id]
                 result_container = network_output.routingActivationMatrices
                 results_array_shape = (*self.model.pathCounts[:(block_id + 1)],
-                                       batch_size, self.model.pathCounts[block_id + 1])
+                                       data_size, self.model.pathCounts[block_id + 1])
             # Loss calculation blocks
             else:
                 output_array = outputs_dict["list_of_logits_unified"]
                 result_container = network_output.logits
-                results_array_shape = (*self.model.pathCounts[:(block_id + 1)], batch_size, self.model.numClasses)
+                results_array_shape = (*self.model.pathCounts[:(block_id + 1)], data_size, self.model.numClasses)
 
             interpreted_results_array = np.zeros(shape=results_array_shape, dtype=np.float)
             interpreted_results_array[:] = np.nan
             route_combinations = Utilities.create_route_combinations(shape_=self.model.pathCounts[:(block_id + 1)])
             for route_combination in route_combinations:
-                for i_, (x, y) in enumerate(dataloader):
+                for i_, (x, y) in tqdm(enumerate(dataloader)):
                     route_offset = self.get_start_offset_for_gather_scatter_model(route_=route_combination,
-                                                                                  batch_id_=i_)
+                                                                                  batch_id_=i_,
+                                                                                  curr_batch_size=x.shape[0])
                     route_activations_array = output_array[route_offset:route_offset + x.shape[0], :]
                     interpreted_results_array[route_combination
-                    ][i_*self.model.batchSize:i_*self.model.batchSize + x.shape[0]] = route_activations_array
+                    ][i_ * self.model.batchSize:i_ * self.model.batchSize + x.shape[0]] = route_activations_array
             assert np.sum(np.isnan(route_combinations)) == 0
             result_container.append(interpreted_results_array)
         return network_output
@@ -92,8 +92,8 @@ class MultiplePathBayesianOptimizer(BayesianOptimizer):
             network_output = self.interpret_gather_scatter_model_outputs(outputs_dict=outputs_dict,
                                                                          dataloader=dataloader)
             block_outputs_complete, routing_matrices_soft_complete, \
-            routing_matrices_hard_complete, \
-            routing_activations_complete, logits_complete = \
+                routing_matrices_hard_complete, \
+                routing_activations_complete, logits_complete = \
                 self.model.validate_v2(dataloader,
                                        temperature=0.1,
                                        enforced_hard_routing_kind="EnforcedRouting")
@@ -101,44 +101,65 @@ class MultiplePathBayesianOptimizer(BayesianOptimizer):
                 if len(k) == 0:
                     continue
                 arr_to_compare = network_output.routingActivationMatrices[len(k) - 1][k]
-                assert np.allclose(v, arr_to_compare)
-                print("X")
+                assert np.allclose(v, arr_to_compare, rtol=1e-3)
+            for k, v in logits_complete.items():
+                if len(k) == 0:
+                    continue
+                arr_to_compare = network_output.logits[0][k]
+                assert np.allclose(v, arr_to_compare, rtol=1e-3)
 
-    # def create_outputs(self, dataloader, repeat_count):
-    #     max_branch_count = np.prod(self.model.pathCounts)
-    #     for path_count in self.model.pathCounts[1:]:
-    #         self.model.enforcedRoutingMatrices.append(
-    #             torch.ones(size=(max_branch_count * self.model.batchSize, path_count), dtype=torch.int64))
-    #
-    #     network_outputs = []
-    #     network_outputs_v2 = []
-    #     for epoch_id in range(repeat_count):
-    #         output_file_path = "test_{0}_outputs_dict.sav".format(
-    #             epoch_id) if dataloader.dataset.train == False else "train_{0}_outputs_dict.sav".format(epoch_id)
-    #         if os.path.isfile(output_file_path):
-    #             outputs_dict = Utilities.pickle_load_from_file(path=output_file_path)
-    #         else:
-    #             outputs_dict = self.model.validate(loader=dataloader, epoch=0, temperature=0.1,
-    #                                                enforced_hard_routing_kind="EnforcedRouting",
-    #                                                return_network_outputs=True, data_kind="test")
-    #             Utilities.pickle_save_to_file(path=output_file_path, file_content=outputs_dict)
-    #
-    #         outputs_dict_v2 = self.model.validate_v2(loader=dataloader, epoch=0, temperature=0.1,
-    #                                                  enforced_hard_routing_kind="EnforcedRouting",
-    #                                                  return_network_outputs=True, data_kind="test")
-    #
-    #         if isinstance(self.model, CigtIgGatherScatterImplementation):
-    #             network_output = self.interpret_gather_scatter_model_outputs(outputs_dict=outputs_dict)
-    #             network_outputs.append(network_output)
-    #         else:
-    #             raise NotImplementedError()
-    #     complete_output = NetworkOutput()
-    #     for block_id in range(len(self.model.pathCounts) - 1):
-    #         total_arr = np.concatenate([n_o.routingActivationMatrices[block_id] for n_o in network_outputs],
-    #                                    axis=block_id + 1)
-    #         complete_output.routingActivationMatrices.append(total_arr)
-    #     total_arr = np.concatenate([n_o.logits[0] for n_o in network_outputs], axis=len(self.model.pathCounts))
-    #     complete_output.logits.append(total_arr)
+    def create_outputs(self, dataloader, repeat_count):
+        network_outputs = []
+        data_kind = "test" if not dataloader.dataset.train else "train"
+        for epoch_id in range(repeat_count):
+            print("Processing Data:{0} Epoch:{1}".format(data_kind, epoch_id))
+            output_file_path = "{0}_{1}_outputs_dict.sav".format(data_kind, epoch_id)
+            if os.path.isfile(output_file_path):
+                epoch_results = Utilities.pickle_load_from_file(path=output_file_path)
+                raw_outputs_dict = epoch_results["raw_outputs_dict"]
+                interpreted_output = epoch_results["interpreted_output"]
+            else:
+                raw_outputs_dict = self.model.validate(loader=dataloader, epoch=0, temperature=0.1,
+                                                       enforced_hard_routing_kind="EnforcedRouting",
+                                                       return_network_outputs=True, data_kind=data_kind)
+
+                if isinstance(self.model, CigtIgGatherScatterImplementation):
+                    interpreted_output = self.interpret_gather_scatter_model_outputs(outputs_dict=raw_outputs_dict,
+                                                                                     dataloader=dataloader)
+                else:
+                    raise NotImplementedError()
+
+                Utilities.pickle_save_to_file(path=output_file_path, file_content={
+                    "raw_outputs_dict": raw_outputs_dict, "interpreted_output": interpreted_output})
+            network_outputs.append(interpreted_output)
+
+        complete_output = NetworkOutput()
+        for block_id in range(len(self.model.pathCounts) - 1):
+            assert len(set([n_o.routingActivationMatrices[block_id].shape[block_id + 1]
+                            for n_o in network_outputs])) == 1
+            total_arr = np.concatenate([n_o.routingActivationMatrices[block_id] for n_o in network_outputs],
+                                       axis=block_id + 1)
+            complete_output.routingActivationMatrices.append(total_arr)
+        assert len(set([n_o.logits[0].shape[len(self.model.pathCounts)] for n_o in network_outputs])) == 1
+        total_arr = np.concatenate([n_o.logits[0] for n_o in network_outputs], axis=len(self.model.pathCounts))
+        complete_output.logits.append(total_arr)
+
+        #     outputs_dict_v2 = self.model.validate_v2(loader=dataloader, epoch=0, temperature=0.1,
+        #                                              enforced_hard_routing_kind="EnforcedRouting",
+        #                                              return_network_outputs=True, data_kind="test")
+        #
+        #     if isinstance(self.model, CigtIgGatherScatterImplementation):
+        #         network_output = self.interpret_gather_scatter_model_outputs(outputs_dict=outputs_dict)
+        #         network_outputs.append(network_output)
+        #     else:
+        #         raise NotImplementedError()
+        # complete_output = NetworkOutput()
+        # for block_id in range(len(self.model.pathCounts) - 1):
+        #     total_arr = np.concatenate([n_o.routingActivationMatrices[block_id] for n_o in network_outputs],
+        #                                axis=block_id + 1)
+        #     complete_output.routingActivationMatrices.append(total_arr)
+        # total_arr = np.concatenate([n_o.logits[0] for n_o in network_outputs], axis=len(self.model.pathCounts))
+        # complete_output.logits.append(total_arr)
 
     # def create_outputs(self, dataloader, repeat_count):
     #     max_branch_count = np.prod(self.model.pathCounts)
