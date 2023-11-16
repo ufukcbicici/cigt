741efc3ee91258d761ce19813e75be80228dac68
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-11-16 16:17:46 +0100

    New implementation that separates CIGT body from policy networks completely.

---
+++ cigt/cigt_output_dataset.py
@@ -0,0 +1,104 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+from auxillary.utilities import Utilities
+from cigt.cigt_ig_gather_scatter_implementation import CigtIgGatherScatterImplementation
+
+
+class CigtOutputDataset(Dataset):
+    def __init__(self, configs):
+        self.listOfFields = []
+        self.dataArrays = {}
+        self.dataLength = None
+        self.inputReductionFactor = configs.policy_networks_cbam_layer_input_reduction_ratio
+
+    def load_from_model(self, model, data_loader, repeat_count, list_of_fields):
+        self.listOfFields = list_of_fields
+        for field_name in self.listOfFields:
+            self.dataArrays[field_name] = {}
+
+        # valid_route_combinations = set()
+        # all_route_combinations = []
+        # for layer_id in range(len(model.pathCounts)):
+        #     paths = model.pathCounts[:(layer_id + 1)]
+        #     combinations = Utilities.create_route_combinations(shape_=paths)
+        #     all_route_combinations.extend(combinations)
+        #
+        # for comb in all_route_combinations:
+        #     if len(comb) < len(model.pathCounts):
+        #         valid_route_combinations.add(comb)
+
+        model.eval()
+        assert isinstance(model, CigtIgGatherScatterImplementation)
+
+        if self.inputReductionFactor > 1:
+            conv_block_reduction_layer = nn.MaxPool2d(kernel_size=self.inputReductionFactor,
+                                                      stride=self.inputReductionFactor)
+        else:
+            conv_block_reduction_layer = nn.Identity()
+
+        for epoch_id in range(repeat_count):
+            print("Epoch {0} is starting.".format(epoch_id))
+            for i, (input_, target) in tqdm(enumerate(data_loader)):
+                with torch.no_grad():
+                    input_var = torch.autograd.Variable(input_).to(model.device)
+                    target_var = torch.autograd.Variable(target).to(model.device)
+                    batch_size = input_var.size(0)
+                    cigt_outputs = model.forward_v2(x=input_var, labels=target_var, temperature=1.0)
+                    for field_name in self.listOfFields:
+                        outputs_dict = cigt_outputs[field_name]
+                        for k, v in outputs_dict.items():
+                            # Skip block outputs for the last layer, these are not used.
+                            if field_name == "block_outputs_dict" and (len(k) == len(model.pathCounts) or k == ()):
+                                continue
+                            if k not in self.dataArrays[field_name]:
+                                self.dataArrays[field_name][k] = []
+
+                            if field_name == "block_outputs_dict":
+                                v_reduced = conv_block_reduction_layer(v)
+                                self.dataArrays[field_name][k].append(v_reduced.detach().cpu().numpy())
+                            else:
+                                self.dataArrays[field_name][k].append(v.detach().cpu().numpy())
+        # Merge all outputs from all iterations into large arrays
+        merged_dict = {}
+        for field_name in self.listOfFields:
+            merged_dict[field_name] = {}
+            for k, v in self.dataArrays[field_name].items():
+                merged_dict[field_name][k] = np.concatenate(v, axis=0)
+        self.dataArrays = merged_dict
+
+        # Delete redundant label arrays
+        for k in self.dataArrays["labels_dict"]:
+            assert np.array_equal(self.dataArrays["labels_dict"][()], self.dataArrays["labels_dict"][k])
+        self.dataArrays["labels_dict"] = {(): self.dataArrays["labels_dict"][()]}
+
+        data_lengths = set()
+        for field_name in self.listOfFields:
+            for k, v in self.dataArrays[field_name].items():
+                data_lengths.add(v.shape[0])
+
+        assert len(data_lengths) == 1
+        self.dataLength = list(data_lengths)[0]
+
+    def save(self, file_path):
+        Utilities.pickle_save_to_file(path=file_path, file_content=self)
+
+    def load_from_file(self, file_path):
+        obj = Utilities.pickle_load_from_file(path=file_path)
+        self.listOfFields = obj.listOfFields
+        self.dataArrays = obj.dataArrays
+        self.dataLength = obj.dataLength
+
+    def __len__(self):
+        return self.dataLength
+
+    def __getitem__(self, idx):
+        sampled_dict = {}
+        for field_name in self.listOfFields:
+            sampled_dict[field_name] = {}
+            for k, v in self.dataArrays[field_name].items():
+                sampled_dict[field_name][k] = v[idx]
+        return sampled_dict
---
+++ cigt_output_dataset_entry.py
@@ -0,0 +1,159 @@
+import os
+
+from randaugment import RandAugment
+from torchvision import datasets
+from torchvision.transforms import transforms
+
+from auxillary.db_logger import DbLogger
+from cigt.cigt_ig_gather_scatter_implementation import CigtIgGatherScatterImplementation
+from cigt.cigt_ig_refactored import CigtIgHardRoutingX
+from cigt.cigt_output_dataset import CigtOutputDataset
+from cigt.cigt_reinforce_multipath import CigtReinforceMultipath
+from cigt.cigt_reinforce_v2 import CigtReinforceV2
+from cigt.cutout_augmentation import CutoutPIL
+from cigt.multipath_evaluator import MultipathEvaluator
+from cigt.multipath_inference_bayesian import MultiplePathBayesianOptimizer
+# from configs.fashion_lenet_cigt_configs import FashionLenetCigtConfigs
+import torch
+
+from cigt.multipath_inference_cross_entropy import MultipathInferenceCrossEntropy
+from cigt.softmax_decay_algorithms.step_wise_decay_algorithm import StepWiseDecayAlgorithm
+from configs.cifar10_resnet_cigt_configs import Cifar10ResnetCigtConfigs
+
+# random.seed(53)
+# np.random.seed(61)
+
+if __name__ == "__main__":
+    print("X")
+    # 5e-4,
+    # 0.0005
+    Cifar10ResnetCigtConfigs.layer_config_list = [
+        {"path_count": 1,
+         "layer_structure": [{"layer_count": 9, "feature_map_count": 16}]},
+        {"path_count": 2,
+         "layer_structure": [{"layer_count": 9, "feature_map_count": 12},
+                             {"layer_count": 18, "feature_map_count": 16}]},
+        {"path_count": 4,
+         "layer_structure": [{"layer_count": 18, "feature_map_count": 16}]}]
+
+    Cifar10ResnetCigtConfigs.backbone = "ResNet"
+    Cifar10ResnetCigtConfigs.input_dims = (3, 32, 32)
+
+    # Thin Baseline
+    # CigtConstants.layer_config_list = [
+    #     {"path_count": 1,
+    #      "layer_structure": [{"layer_count": 9, "feature_map_count": 16},
+    #                          {"layer_count": 9, "feature_map_count": 12},
+    #                          {"layer_count": 18, "feature_map_count": 16},
+    #                          {"layer_count": 18, "feature_map_count": 16}]}]
+
+    # Thick Baseline
+    # CigtConstants.layer_config_list = [
+    #     {"path_count": 1,
+    #      "layer_structure": [{"layer_count": 18, "feature_map_count": 16},
+    #                          {"layer_count": 18, "feature_map_count": 32},
+    #                          {"layer_count": 18, "feature_map_count": 64}]}]
+
+    Cifar10ResnetCigtConfigs.classification_wd = 0.0005
+    Cifar10ResnetCigtConfigs.information_gain_balance_coeff_list = [5.0, 5.0]
+    Cifar10ResnetCigtConfigs.loss_calculation_kind = "MultipleLogitsMultipleLosses"
+    Cifar10ResnetCigtConfigs.enable_information_gain_during_warm_up = True
+    Cifar10ResnetCigtConfigs.enable_strict_routing_randomization = False
+    Cifar10ResnetCigtConfigs.routing_randomization_ratio = 0.5
+    Cifar10ResnetCigtConfigs.warm_up_kind = "RandomRouting"
+    Cifar10ResnetCigtConfigs.decision_drop_probability = 0.5
+    Cifar10ResnetCigtConfigs.number_of_cbam_layers_in_routing_layers = 3
+    Cifar10ResnetCigtConfigs.cbam_reduction_ratio = 4
+    Cifar10ResnetCigtConfigs.cbam_layer_input_reduction_ratio = 4
+    Cifar10ResnetCigtConfigs.apply_relu_dropout_to_decision_layer = False
+    Cifar10ResnetCigtConfigs.decision_dimensions = [128, 128]
+    Cifar10ResnetCigtConfigs.apply_mask_to_batch_norm = False
+    Cifar10ResnetCigtConfigs.advanced_augmentation = True
+    Cifar10ResnetCigtConfigs.use_focal_loss = False
+    Cifar10ResnetCigtConfigs.focal_loss_gamma = 2.0
+    Cifar10ResnetCigtConfigs.batch_norm_type = "BatchNorm"
+    Cifar10ResnetCigtConfigs.data_parallelism = False
+
+    Cifar10ResnetCigtConfigs.softmax_decay_controller = StepWiseDecayAlgorithm(
+        decay_name="Stepwise",
+        initial_value=Cifar10ResnetCigtConfigs.softmax_decay_initial,
+        decay_coefficient=Cifar10ResnetCigtConfigs.softmax_decay_coefficient,
+        decay_period=Cifar10ResnetCigtConfigs.softmax_decay_period,
+        decay_min_limit=Cifar10ResnetCigtConfigs.softmax_decay_min_limit)
+
+    kwargs = {'num_workers': 0, 'pin_memory': True}
+    heavyweight_augmentation = transforms.Compose([
+        transforms.Resize((32, 32)),
+        CutoutPIL(cutout_factor=0.5),
+        RandAugment(),
+        transforms.ToTensor(),
+    ])
+    lightweight_augmentation = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+    ])
+    train_loader_hard = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../data', download=True, train=True, transform=heavyweight_augmentation),
+        batch_size=Cifar10ResnetCigtConfigs.batch_size, shuffle=False, **kwargs)
+    test_loader_light = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../data', download=True, train=False, transform=lightweight_augmentation),
+        batch_size=Cifar10ResnetCigtConfigs.batch_size, shuffle=False, **kwargs)
+
+    chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "checkpoints/cigtlogger2_75_epoch1575.pth")
+    # chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "checkpoints/dblogger_331_epoch11.pth")
+    data_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "cigtlogger2_75_epoch1575")
+
+    DbLogger.log_db_path = DbLogger.jr_cigt
+
+    model = CigtIgGatherScatterImplementation(
+        run_id=-1,
+        model_definition="Gather Scatter Cigt With CBAM Routers With Random Augmentation - cbam_layer_input_reduction_ratio:4  - [1,2,4] - [5.0, 5.0] - number_of_cbam_layers_in_routing_layers:3 - MultipleLogitsMultipleLosses - Wd:0.0006 - 350 Epoch Warm up with: RandomRoutingButInformationGainOptimizationEnabled - InformationGainRoutingWithRandomization",
+        num_classes=10,
+        configs=Cifar10ResnetCigtConfigs)
+    model.to(model.device)
+    model.execute_forward_with_random_input()
+    checkpoint = torch.load(chck_path, map_location=model.device)
+    load_result = model.load_state_dict(state_dict=checkpoint["model_state_dict"], strict=False)
+
+    test_cigt_output_dataset = CigtOutputDataset(configs=Cifar10ResnetCigtConfigs)
+    test_cigt_output_dataset.load_from_model(model=model, data_loader=test_loader_light, repeat_count=1,
+                                             list_of_fields=["block_outputs_dict", "routing_matrices_soft_dict",
+                                                             "labels_dict", "logits_dict"])
+    test_cigt_output_dataset.save(file_path="test_cigt_dataset.sav")
+
+    train_cigt_output_dataset = CigtOutputDataset(configs=Cifar10ResnetCigtConfigs)
+    train_cigt_output_dataset.load_from_model(model=model, data_loader=train_loader_hard, repeat_count=10,
+                                              list_of_fields=["block_outputs_dict", "routing_matrices_soft_dict",
+                                                              "labels_dict", "logits_dict"])
+    train_cigt_output_dataset.save(file_path="train_cigt_dataset.sav")
+
+    # run_id = DbLogger.get_run_id()
+    # model = CigtReinforceV2(
+    #     configs=Cifar10ResnetCigtConfigs,
+    #     model_definition="Reinforce Multipath CIGT",
+    #     num_classes=10,
+    #     run_id=run_id,
+    #     model_mac_info=mac_counts_per_block,
+    #     is_debug_mode=True)
+    # model.to(model.device)
+    # model.execute_forward_with_random_input()
+    # checkpoint = torch.load(chck_path, map_location=model.device)
+    # load_result = model.load_state_dict(state_dict=checkpoint["model_state_dict"], strict=False)
+    # for param_name in load_result.missing_keys:
+    #     block_check1 = [param_name.startswith("policyNetworks.{0}".format(block_id))
+    #                     or param_name.startswith("valueNetworks.{0}".format(block_id))
+    #                     for block_id in range(len(model.pathCounts[1:]))]
+    #     assert any(block_check1)
+    #     block_check2 = ["block_{0}".format(block_id) in param_name for block_id in range(len(model.pathCounts[1:]))]
+    #     assert any(block_check2)
+    #
+    # model.modelFilesRootPath = Cifar10ResnetCigtConfigs.model_file_root_path_paperspace
+    # explanation = model.get_explanation_string()
+    # DbLogger.write_into_table(rows=[(run_id, explanation)], table=DbLogger.runMetaData)
+    #
+    # # model.validate(loader=train_loader, data_kind="train", epoch=0, temperature=0.1)
+    # # model.validate(loader=test_loader, data_kind="test", epoch=0, temperature=0.1)
+    #
+    # # train_dict = model.validate(loader=train_loader_hard, epoch=0, data_kind="train", temperature=1.0)
+    # # test_dict = model.validate(loader=test_loader_light, epoch=0, data_kind="test", temperature=1.0)
+    # model.fit_policy_network(train_loader=train_loader_hard, test_loader=test_loader_light)
