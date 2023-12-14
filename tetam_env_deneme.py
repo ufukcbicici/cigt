645e6301eb0c59b1fc0e7ed0a6c13155f027757d
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-12-14 18:57:26 +0100

    Q Learning implementation for CIGT.

--- tetam_env_deneme.py
+++ tetam_env_deneme.py
@@ -16,3 +16,12 @@ print(np.mean(y))
 DbLogger.log_db_path = DbLogger.hpc_docker1
 run_id = DbLogger.get_run_id()
 print("RunId is:{0}".format(run_id))
+DbLogger.write_into_table(
+    rows=[(run_id,
+           42,
+           67,
+           94.32,
+           0.054,
+           94.38,
+           0.062,
+           0.1245)], table=DbLogger.logsTableQCigt)
