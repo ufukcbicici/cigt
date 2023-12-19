d889e14ca11d74df6be7d19b28cac6a168cacc49
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-12-19 17:19:40 +0100

    Q Learning implementation for CIGT.

--- auxillary/db_logger.py
+++ auxillary/db_logger.py
@@ -60,6 +60,7 @@ class DbLogger:
     jr_cigt = "C://Users//ufuk.bicici//PycharmProjects//cigt//dblogger.db"
     jr_paperspace_cigt = "C://Users//ufuk.bicici//PycharmProjects//cigt//paperspace_dblogger.db"
     hpc_docker1 = "/home/cigt/dblogger.db"
+    hpc_docker2 = "/home/cigt/dblogger_2.db"
 
     log_db_path = None
 
