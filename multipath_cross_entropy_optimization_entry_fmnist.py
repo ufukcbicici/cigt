57a96a6135b5f3a154db98b1e09b7baed0954f9f
ufuk.bicici <ufuk.bicici@josephinum.at>
2023-12-26 00:34:38 +0100

    Experiments of a new CIFAR 10 CIGT model for Cross Entropy Search.

--- multipath_cross_entropy_optimization_entry_fmnist.py
+++ multipath_cross_entropy_optimization_entry_fmnist.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     DbLogger.log_db_path = DbLogger.paperspace
 
     chck_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "checkpoints/cigtlogger2_160_epoch145.pth")
-    data_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "cigtlogger2_160_epoch145")
+    data_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "cigtlogger2_160_epoch145_data")
 
     if not os.path.isdir(data_path):
         os.mkdir(data_path)
