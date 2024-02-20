ba97f15fb7f67ac40a57b0b1bccaf12e55fa91d0
ufuk.bicici <ufuk.bicici@josephinum.at>
2024-02-20 12:03:11 +0100

    End to end Q-CIGT

--- q_net_entry_points/cifar10_end_2_end_q_learning_grid_search_entry.py
+++ q_net_entry_points/cifar10_end_2_end_q_learning_grid_search_entry.py
@@ -104,8 +104,8 @@ if __name__ == "__main__":
     # wd_list = [0.0, 0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01] * 5
     # mac_lambda_list = sorted(mac_lambda_list)
     # wd_list = sorted(wd_list)
-    lr_list = [0.01]
-    mac_lambda_list = sorted([0.01, 0.1] * 5)
+    lr_list = sorted([0.005, 0.01] * 5)
+    mac_lambda_list = sorted([0.01])
     param_grid = Utilities.get_cartesian_product(list_of_lists=[lr_list, mac_lambda_list])
     for iteration_id, params in enumerate(param_grid):
         print("Iteration:{0}".format(iteration_id))
@@ -189,11 +189,14 @@ if __name__ == "__main__":
         # Grid search params
         QCigtCifar10Configs.initial_lr = params[0]
         QCigtCifar10Configs.policy_networks_mac_lambda = params[1]
+        QCigtCifar10Configs.classification_wd = 0.0005
 
         model = CigtQlearningEnd2End(
             configs=QCigtCifar10Configs,
-            model_definition="Q Learning CIGT - LSTM 32 Lr {0} Mac Lambda:{1}".format(
-                QCigtCifar10Configs.initial_lr, QCigtCifar10Configs.policy_networks_mac_lambda),
+            model_definition="Q Learning CIGT - LSTM 32 Lr {0} Mac Lambda:{1} Classification Wd:{2}".format(
+                QCigtCifar10Configs.initial_lr,
+                QCigtCifar10Configs.policy_networks_mac_lambda,
+                QCigtCifar10Configs.classification_wd),
             num_classes=10,
             run_id=run_id,
             model_mac_info=mac_counts_per_block,
